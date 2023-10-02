29659b40014ee3058bde318ee1f4e52249c1714e
Warren <warrenroque17@gmail.com>
2023-10-02 11:37:49 -0400

    Create playRound function to compare computer/player input

--- app.js
+++ app.js
@@ -2,13 +2,43 @@
 
 function getComputerChoice() {
   let randomNumber = Math.floor(Math.random() * (2 + 1));
-  console.log(randomNumber);
   return randomNumber == 2 ? "ROCK" : randomNumber == 1 ? "PAPER" : "SCISSORS";
 }
 
-console.log(getComputerChoice());
-console.log(getComputerChoice());
-console.log(getComputerChoice());
-console.log(getComputerChoice());
-console.log(getComputerChoice());
-console.log(getComputerChoice());
+// 2. Create function that simulates one round by taking in both inputs.
+let playerSelection = prompt("Rock, paper or scissors?").toUpperCase();
+let computerSelection = getComputerChoice();
+
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection == computerSelection) {
+    console.log("It's a tie!");
+    return "TIE"
+  }
+  if (playerSelection == "ROCK" & computerSelection == "PAPER") {
+    console.log("You lose!");
+    return "LOSE"
+  }
+  if (playerSelection == "ROCK" & computerSelection == "SCISSORS") {
+    console.log("You win!");
+    return "WIN"
+  }
+  if (playerSelection == "PAPER" & computerSelection == "SCISSORS") {
+    console.log("You lose!");
+    return "LOSE"
+  }
+  if (playerSelection == "PAPER" & computerSelection == "ROCK") {
+    console.log("You win!");
+    return "WIN"
+  }
+  if(playerSelection == "SCISSORS" & computerSelection == "ROCK") {
+    console.log("You lose!")
+    return "LOSE"
+  }
+  if (playerSelection == "SCISSORS" & computerSelection == "PAPER") {
+    console.log("You win!");
+    return "WIN"
+  }
+}
+
+
+playRound(playerSelection,computerSelection);
