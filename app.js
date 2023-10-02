2a4f5c7bcdcf0fbbe952381369471fcf516d3344
Warren <warrenroque17@gmail.com>
2023-10-02 13:40:02 -0400

    Add function to play multiple rounds with a score counter

--- app.js
+++ app.js
@@ -1,44 +1,58 @@
 // 1. Create function that randomly returns rock, paper or scissors.
+// 2. Create function that simulates one round by taking in both inputs.
+// 3. Create function that allows to play X amount of rounds while saving score.
 
 function getComputerChoice() {
   let randomNumber = Math.floor(Math.random() * (2 + 1));
   return randomNumber == 2 ? "ROCK" : randomNumber == 1 ? "PAPER" : "SCISSORS";
 }
 
-// 2. Create function that simulates one round by taking in both inputs.
-let playerSelection = prompt("Rock, paper or scissors?").toUpperCase();
-let computerSelection = getComputerChoice();
+// let playerSelection = prompt("Rock, paper or scissors?").toUpperCase();
+// let computerSelection = getComputerChoice();
+let result = 0;
 
 function playRound(playerSelection, computerSelection) {
   if (playerSelection == computerSelection) {
     console.log("It's a tie!");
-    return "TIE"
+    return result
   }
   if (playerSelection == "ROCK" & computerSelection == "PAPER") {
     console.log("You lose!");
-    return "LOSE"
+    return result--
   }
   if (playerSelection == "ROCK" & computerSelection == "SCISSORS") {
     console.log("You win!");
-    return "WIN"
+    return result++
   }
   if (playerSelection == "PAPER" & computerSelection == "SCISSORS") {
     console.log("You lose!");
-    return "LOSE"
+    return result--
   }
   if (playerSelection == "PAPER" & computerSelection == "ROCK") {
     console.log("You win!");
-    return "WIN"
+    return result++
   }
   if(playerSelection == "SCISSORS" & computerSelection == "ROCK") {
     console.log("You lose!")
-    return "LOSE"
+    return result--
   }
   if (playerSelection == "SCISSORS" & computerSelection == "PAPER") {
     console.log("You win!");
-    return "WIN"
+    return result++
   }
 }
 
+function game(rounds) {
+  for (let i=0; i < rounds; i++) {
+    let playerSelection = prompt("Rock, paper or scissors?").toUpperCase();
+    let computerSelection = getComputerChoice();
+    
+    playRound(playerSelection, computerSelection);
 
-playRound(playerSelection,computerSelection);
+    // if statement so score doesn't go below 0
+    result < 0 ? result = 0 : result; 
+    console.log(result);
+  }
+}
+
+game(5);
