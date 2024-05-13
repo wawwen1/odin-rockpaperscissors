96a8230c9039ce92e42ac9367125feb25f12150a
Warren <warrenroque17@gmail.com>
2024-05-13 16:04:44 -0400

    Add result and score function, buttons and modify playRound functino

--- app.js
+++ app.js
@@ -10,47 +10,71 @@ function getComputerChoice() {
 let score = 0;
 
 function playRound(playerSelection, computerSelection) {
+  let resultMessage;
   if (playerSelection == computerSelection) {
-    console.log("It's a tie!");
-    return score
+    resultMessage = "It's a tie!";
+    score;
   }
-  if (playerSelection == "ROCK" & computerSelection == "PAPER") {
-    console.log("You lose!");
-    return score--
+  if ((playerSelection == "ROCK") & (computerSelection == "PAPER")) {
+    resultMessage = "You lose!";
+    score--;
   }
-  if (playerSelection == "ROCK" & computerSelection == "SCISSORS") {
-    console.log("You win!");
-    return score++
+  if ((playerSelection == "ROCK") & (computerSelection == "SCISSORS")) {
+    resultMessage = "You win!";
+    score++;
   }
-  if (playerSelection == "PAPER" & computerSelection == "SCISSORS") {
-    console.log("You lose!");
-    return score--
+  if ((playerSelection == "PAPER") & (computerSelection == "SCISSORS")) {
+    resultMessage = "You lose!";
+    score--;
   }
-  if (playerSelection == "PAPER" & computerSelection == "ROCK") {
-    console.log("You win!");
-    return score++
+  if ((playerSelection == "PAPER") & (computerSelection == "ROCK")) {
+    resultMessage = "You win!";
+    score++;
   }
-  if(playerSelection == "SCISSORS" & computerSelection == "ROCK") {
-    console.log("You lose!")
-    return score--
+  if ((playerSelection == "SCISSORS") & (computerSelection == "ROCK")) {
+    resultMessage = "You lose!";
+    score--;
   }
-  if (playerSelection == "SCISSORS" & computerSelection == "PAPER") {
-    console.log("You win!");
-    return score++
+  if ((playerSelection == "SCISSORS") & (computerSelection == "PAPER")) {
+    resultMessage = "You win!";
+    score++;
   }
+  gameResult(resultMessage);
 }
 
 function game(rounds) {
-  for (let i=0; i < rounds; i++) {
+  for (let i = 0; i < rounds; i++) {
     let playerSelection = prompt("Rock, paper or scissors?").toUpperCase();
     let computerSelection = getComputerChoice();
-    
+
     playRound(playerSelection, computerSelection);
 
     // if statement so score doesn't go below 0
-    score < 0 ? score = 0 : score; 
+    score < 0 ? (score = 0) : score;
     console.log(score);
   }
 }
 
-game(5);
+const gameResult = (message) => {
+  let resultDiv = document.querySelector("result");
+  resultDiv.textContent = message;
+};
+
+const updateScore = () => {
+  let scoreDiv = document.querySelector("score");
+  scoreDiv.textContent = "Score: " + score;
+};
+
+document.querySelector("#rockButton").addEventListener("click", function () {
+  console.log("rock");
+});
+
+document.querySelector("#paperButton").addEventListener("click", function () {
+  console.log("paper");
+});
+
+document
+  .querySelector("#scissorsButton")
+  .addEventListener("click", function () {
+    console.log("scissors");
+  });
